4c41717c3eca09c5f0fa6861600177f008a9bc28
angel-1006 <sonarobert1006@gmail.com>
2026-03-28 07:17:20 -0500

    Add 3 synthetic test samples for analysis

---
+++ test_samples/sample_suspicious.ps1
@@ -0,0 +1,6 @@
+# Synthetic test script - not real malware
+# roll_number: 727823TUCY045
+powershell -EncodedCommand dABlAHMAdAA=
+$url = "http://192.168.1.100/payload"
+GetAsyncKeyState keystroke logger test
+HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run
